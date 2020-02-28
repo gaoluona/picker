10c46d4ba0d9ce779c5734d92b48bf87a02016c8
gaoluona <214861919@qq.com>
2020-02-28 17:41:25 +0800

    fia pickerAddress

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ export { default as Picker } from './picker';
 
 export { default as Popup } from './popup';
 
+export { default as PickerAddress } from '../components-ext/picker-address';
+
 export { default as BaseModal } from './modal/BaseModal';
